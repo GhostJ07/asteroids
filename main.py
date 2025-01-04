0c4dec05c470f0aeaa106787b13a6d27ffe2428e
Ghostj07 <Gilles.decock@hotmail.com>
2025-01-04 20:01:45 +0000

    L8 Groups

--- main.py
+++ main.py
@@ -16,18 +16,27 @@ def main():
     pygame.init(); #Initialising pygame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)); #setting the screen size of the 'window'
     clock = pygame.time.Clock() #creating a clock to make 60fps
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     dt = 0
     
     while True : #creating an infinite game loop
         for event in pygame.event.get(): #This will check if the user has closed the window and exit the game loop if they do. It will make the window's close button work.
             if event.type == pygame.QUIT:
                 return
-        player.update(dt) #make the player rotate
 
+        for things in updatable:    
+            things.update(dt)   #make the player rotate
 
         screen.fill("black") #filling that window in black
-        player.draw(screen) #add tje player on the screen
+        for things in drawable:
+            things.draw(screen) #add the player on the screen
         pygame.display.flip() #Use pygame's display.flip() method to refresh the screen. Be sure to call this last!
 
         dt = clock.tick(60) / 1000 # limit the framerate to 60 FPS
